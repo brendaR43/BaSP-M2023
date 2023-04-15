fcd4a4cc64371fb1361808e8d0535bc73e136ada
brendaR43 <brenromero43@gmail.com>
2023-04-15 15:04:36 -0300

    BaSP-W05-2023: added Week05, added exercises folder and finalization of the first exercises in the folder 01-variables-and-operators.js

---
+++ Week-05/exercises/01-variables-and-operators.js
@@ -0,0 +1,22 @@
+//Ejercicio 1. Variables y Operadores
+
+//a
+var a= 59;
+var b= 92;
+var c= a + b;
+console.log(c);
+
+//b
+var firstName= 'Brenda';
+var lastName= 'Romero';
+var fullName= firstName + ' ' + lastName;
+console.log(fullName);
+
+//c
+var adress= 'España 856';
+var city= 'Rosario';
+var datalenght= adress.length + city.length;
+console.log(datalenght);
+
+
+
